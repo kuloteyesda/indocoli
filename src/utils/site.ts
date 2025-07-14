5a6d02a776c87b837e0e07f6114f7ef292ebe946
kuloteyesda <kuloteyesda@gmail.com>
2025-07-15 00:41:30 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Indo Coli';
-export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Coli Sma Cindo Tante China Terbaru Viral';
-export const url = 'https://indocoli.pages.dev';
-export const terbit = '2025-06-27T00:01:03Z';
